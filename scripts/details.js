adc3b523637900ed3f9f008c41d89112e3a5186d
Sirius-Development <griffinferguson.us@gmail.com>
2021-09-17 14:52:47 -0500

    update(script): Continue work on dynamic details

--- scripts/details.js
+++ scripts/details.js
@@ -2,16 +2,25 @@
 function details() {
 	const currentWeather = document.getElementById('detail_card1').innerText;
 	var icon = document.getElementById('icon');
+	var bgcolor;
+	var bgcolorBase = [44, 160, 255];
 
 	if (currentWeather == 'Clear') {
 		icon.href = '/images/sunny.svg';
+		bgcolor = [44, 160, 255]
 	} else if (currentWeather == 'Partly Cloudy') {
 		icon.href = '/images/partly.svg';
+		bgcolor = [139, 176, 222]
 	} else if (currentWeather == 'Overcast') {
 		icon.href = '/images/cloudy.svg';
+		bgcolor = [105, 105, 105]
 	} else if (currentWeather == 'Rainy') {
 		icon.href = '/images/rainy.svg';
+		bgcolor = [12, 7, 90]
 	} else if (currentWeather == 'Thunderstorms') {
 		icon.href = '/images/thunder.svg';
+		bgcolor = [0, 0, 0]
 	}
+	
+	document.title = `Weather for ${document.getElementById('location').innerText}`
 }
